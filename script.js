e91a4015daa3c03ae12d6d4f86ebf49915092876
kodego-mae <funj451@gmail.com>
2023-03-29 03:48:09 +0700

    updated js script and css files

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 const header = document.querySelector("header");
 
-window,
-  addEventListener("scroll", function () {
-    header.classList.toggle("sticky", window.scrollY > 0);
-  });
+window.addEventListener("scroll", function () {
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
 
 let menu = document.querySelector("#menu-icon");
 let navbar = document.querySelector(".navbar");
@@ -13,12 +12,12 @@ menu.onclick = () => {
   navbar.classList.toggle("active");
 };
 
-windows.onscroll = () => {
+window.onscroll = () => {
   menu.classList.remove("bx-x");
   navbar.classList.remove("active");
 };
 
-const sr = ScrollRevel({
+const sr = ScrollReveal({
   distance: "25px",
   duration: 2500,
   reset: true,
